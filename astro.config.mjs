3064d3b2d8ba4a5c8b68f6b41e41c4bc7e650d59
destampes simon <112078712+destampes@users.noreply.github.com>
2023-05-10 23:56:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
     import { defineConfig } from 'astro/config'
 
     export default defineConfig({
