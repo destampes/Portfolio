a13f1d698e632eaf8b2184661cccfdad87925887
destampes simon <112078712+destampes@users.noreply.github.com>
2023-05-10 23:38:27 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+    import { defineConfig } from 'astro/config'
+
+    export default defineConfig({
+      site: 'https://destampes.github.io',
+      base: '/my-repo',
+    })
